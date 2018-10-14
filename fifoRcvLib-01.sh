8f4fd9a7b51ca001f674673875d51396785461c8
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2018-10-14 17:37:07 -0400

    Update fifoRcvLib-01.sh

--- fifoRcvLib-01.sh
+++ fifoRcvLib-01.sh
@@ -34,32 +34,32 @@ doMsg () { #dd log "doMsg #2  >>$1";
 onRcvLpInit () { log "#Info - onRcvLpInit is NOOP"; }
 
 fifoRcvLp () {  onRcvLpInit
-	local timeOut=false  p='~~~' line='__'  ;
-	while [ -p "$myFifoPFN" ]; do
-#		@@@@          vvvvv  when inactive, progress timeout to longer ???
-		while read -r -t 10 -u $myFifoFD p; do 	line=$line$p
-			if [ "$line" = "QUIT" ]; then # @@@ But not for Central IN, ?????
-				log ">>MsgPipe[$myFifoPFN] - Quit cmd recv'd";     return; 
-			else
-				if [ $timeOut ]; then timeOut=false; # log ''; #1st after TimeOut -> new ln
-				fi
-			#dd	log "MsgPipe[$myFifoPFN] >>$line<<"
-				doMsg "$line";  line='';
-			fi
-		done
-		if [ -z "$p"]; then  #Is this a time out w/o any partial read?
-			timeOut=true; # echo -n "*";  
-			#qq sleep 5;
-		else
-			log "lclMsgRcv.sh - fifoRcvLp: read -> partial, so sleep 1";  sleep 1;
-		fi 
-#qq		timeOut=false
-	done
-	log "[RcvMsg] fifoRcvLp() - Exiting: $myFifoPFN FiFo GONE!"
+    local timeOut=false  p='~~~' line='__'  ;
+    while [ -p "$myFifoPFN" ]; do
+#	@@@@          vvvvv  when inactive, progress timeout to longer ???
+        while read -r -t 10 -u $myFifoFD p; do 	line=$line$p
+            if [ "$line" = "QUIT" ]; then # @@@ But not for Central IN, ?????
+                log ">>MsgPipe[$myFifoPFN] - Quit cmd recv'd";     return; 
+            else
+                if [ $timeOut ]; then timeOut=false; # log ''; #1st after TimeOut -> new ln
+                fi
+         #dd    log "MsgPipe[$myFifoPFN] >>$line<<"
+                doMsg "$line";  line='';
+            fi
+        done
+        if [ -z "$p"]; then  #Is this a time out w/o any partial read?
+            timeOut=true; # echo -n "*";  
+            #qq sleep 5;
+        else
+            log "lclMsgRcv.sh - fifoRcvLp: read -> partial, so sleep 1";  sleep 1;
+        fi 
+#qq	timeOut=false
+    done
+    log "[RcvMsg] fifoRcvLp() - Exiting: $myFifoPFN FiFo GONE!"
 } # ----
 startFifoRcv () { local myCmdDir=$1 myFifoPFN=$2  myFifoFD # FD = File Descriptor
-	trap "rm -f $myFifoPFN" EXIT;  [[ -e $myFifoPFN ]] || mkfifo $myFifoPFN;
-	exec {myFifoFD}<>"$myFifoPFN";  fifoRcvLp;  {myFifoFD}>&- ;  rm -f myFifoPFN;
+    trap "rm -f $myFifoPFN" EXIT;  [[ -e $myFifoPFN ]] || mkfifo $myFifoPFN;
+    exec {myFifoFD}<>"$myFifoPFN";  fifoRcvLp;  {myFifoFD}>&- ;  rm -f myFifoPFN;
 } 
 
 suFifoRcv() { local cmdPth=$1  fifoPth=$2 fifoFN=$3 ;
