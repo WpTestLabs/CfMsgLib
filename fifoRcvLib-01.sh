735ee12cde62b2579bf0ab200c180a2a878d0ff4
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2018-10-15 14:50:01 -0400

    Update fifoRcvLib-01.sh

--- fifoRcvLib-01.sh
+++ fifoRcvLib-01.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
-#	echo "fifoRcvLib.sh";
-
-export LogFQPFN=/srv/log/wkFlo/hstWkFloRcvRAW.txt
-# Example - msg () { echo "$@" >> $LogFQPFN; } # For Knz, add hardlink w/in Kn vw back to main fifo
-
-echo "#### Start: $LogFQPFN ####"  > $LogFQPFN
-log () { echo "`date +%Y/%m/%d-%T` - $@" >>$LogFQPFN; }
-export -f log #@@@ But not in Alpine!!!
-log "[Hst] Starting log for: fifoRcvLib.sh"
+#	echo "fifoRcvLib-01.sh";
 
 die () { log "$@"; exit; }
-##### doMsg () { log "doMsg STUB >>$1"; }
 declare -A CmdMp     # Create an associative array
 
 doMsgA () {  local cmd0=$1; shift;  # log "doMsgA() - cmd: $cmd >< args: $@"
@@ -66,26 +57,3 @@ suFifoRcv() { local cmdPth=$1  fifoPth=$2 fifoFN=$3 ;
     mkdir -p $cmdPth $fifoPth;   cd $cmdPth;
     startFifoRcv $cmdPth  $fifoPth/$fifoFN
 }
-############ End of lib #########
-
-CmdMp[SqlHB]=SqlHB
-CmdMp[TL]=TL
-CmdMp[WkPrxySQL]=WkPrxySQL
-
-SqlHB () { log "[SQL] SqlHB: $@  `cat /proc/uptime`"; }
-TL () { echo "`cat /proc/uptime` -- $@" >> /TimeLine.txt; }
-
-WkPrxySQL () {  log "[WkFlo] Start - WkPrxySQL()  xc: $1   FQHP: $2  FN: $3"
-    if [[ "0" = $1 ]]; then
-    	log "[WkFlo] WkPrxySQL() - Push $2$3 OffSite!"
-    else
-    	log "[WkFlo] WkPrxySQL() - Error exit code: $1"
-    fi
-}
-#######
-
-#qq mkdir -p /srv/run/wkFlo  $Srv/Knz/WkFlo/srv/cmd
-#qq cd $Srv/Knz/WkFlo/srv/cmd
-#qq startFifoRcv $Srv/Knz/WkFlo/srv/cmd /srv/run/wkFlo/hstWkFloRcv.fifo
-
-suFifoRcv $Srv/Knz/WkFlo/srv/cmd /srv/run/wkFlo  hstWkFloRcv.fifo
