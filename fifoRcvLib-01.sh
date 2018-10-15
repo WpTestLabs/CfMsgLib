48cff36331c7810de98faa538c16e37cf93f0d4d
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2018-10-15 11:02:37 -0400

    Update fifoRcvLib-01.sh

--- fifoRcvLib-01.sh
+++ fifoRcvLib-01.sh
@@ -5,7 +5,7 @@ export LogFQPFN=/srv/log/wkFlo/hstWkFloRcvRAW.txt
 # Example - msg () { echo "$@" >> $LogFQPFN; } # For Knz, add hardlink w/in Kn vw back to main fifo
 
 echo "#### Start: $LogFQPFN ####"  > $LogFQPFN
-log () { echo "`date +%Y/%m/%d-%T` - $@" >>$LogPFN; }
+log () { echo "`date +%Y/%m/%d-%T` - $@" >>$LogFQPFN; }
 export -f log #@@@ But not in Alpine!!!
 log "[Hst] Starting log for: fifoRcvLib.sh"
 
@@ -16,7 +16,7 @@ declare -A CmdMp     # Create an associative array
 doMsgA () {  local cmd0=$1; shift;  # log "doMsgA() - cmd: $cmd >< args: $@"
     cmd=${CmdMp[$cmd0]}
     [[ -n "$cmd" ]] && $cmd "$@" && return;
-    log "[WkFlo] doMsgA() - $cmd0 is not an internal command - Trying external commands..."
+    log "[WkFlo] doMsgA() - $cmd0 is not an internal cmd - Trying external cmd's..."
     log "[WkFlo] WkFloRcv - cur dir: $PWD - lib dir: $myCmdDir"
     log "ls>> `ls $myCmdDir`"
     if [[ -e $myCmdDir/$cmd0 ]]; then
@@ -34,26 +34,26 @@ doMsg () { #dd log "doMsg #2  >>$1";
 onRcvLpInit () { log "#Info - onRcvLpInit is NOOP"; }
 
 fifoRcvLp () {  onRcvLpInit
-    local timeOut=false  p='~~~' line='__'  ;
+    local tmOt=false  p='~~~' line='__'  ; # tmOt: timeOut (in sec's)
     while [ -p "$myFifoPFN" ]; do
 #	@@@@          vvvvv  when inactive, progress timeout to longer ???
         while read -r -t 10 -u $myFifoFD p; do 	line=$line$p
             if [ "$line" = "QUIT" ]; then # @@@ But not for Central IN, ?????
                 log ">>MsgPipe[$myFifoPFN] - Quit cmd recv'd";     return; 
             else
-                if [ $timeOut ]; then timeOut=false; # log ''; #1st after TimeOut -> new ln
+                if [ $tmOt ]; then tmOt=false; # log ''; #1st after TimeOut -> new ln
                 fi
          #dd    log "MsgPipe[$myFifoPFN] >>$line<<"
                 doMsg "$line";  line='';
             fi
         done
         if [ -z "$p"]; then  #Is this a time out w/o any partial read?
-            timeOut=true; # echo -n "*";  
+            tmOt=true; # echo -n "*";  
             #qq sleep 5;
         else
             log "lclMsgRcv.sh - fifoRcvLp: read -> partial, so sleep 1";  sleep 1;
         fi 
-#qq	timeOut=false
+#qq	tmOt=false
     done
     log "[RcvMsg] fifoRcvLp() - Exiting: $myFifoPFN FiFo GONE!"
 } # ----
