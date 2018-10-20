acd97c89db7a851d3c5b279bf95db1591937cadd
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2018-10-20 13:40:34 -0400

    Update WkFloSqlRcv.sh

--- WkFloSqlRcv.sh
+++ WkFloSqlRcv.sh
@@ -21,14 +21,13 @@ log "[SQL] Starting log for: WkFloSqlRcv.sh" #@@@@@@@@
 CmdMp[Echo]=Echo
 # CmdMp[TL]=TL
 CmdMp[WkPrxySQL]=WkPrxySQL
-
-# TL () { echo "`cat /proc/uptime` -- $@" >> /TimeLine.txt; }
-
+CmdMp[WkFlo]=WkFloGX
 
 Echo () {  log "[SQL] Echo>> $@"; 
     msg "# [SQL] Echo>> $@"; 
     msg $@;
 }
+# TL () { echo "`cat /proc/uptime` -- $@" >> /TimeLine.txt; }
 
 
 WkPrxySQL () {  log "[WkFlo] Start - WkPrxySQL()  xc: $1   FQHP: $2  FN: $3"
@@ -37,6 +36,29 @@ WkPrxySQL () {  log "[WkFlo] Start - WkPrxySQL()  xc: $1   FQHP: $2  FN: $3"
     else
     	log "[WkFlo] WkPrxySQL() - Error exit code: $1"
     fi
+}  ##############
+export WkFloTkn
+declare -A WfCmdMP
+  WfCmdMP[DbDmp]=DbDmpGX
+
+WkFloGX () { WkFloTkn= $1; local cmdGP=$SrvGP/lib/wkFlo  cmd0=$2  cmd; shift 2;
+  msg "# [SQL] WkFloGX() - Start, tkn: $WkFloTkn  cmd: $cmd0  args: $@ "
+  cmd=${WfCmdMP[$cmd0]};  [[ -n "$cmd" ]] && $cmd "$@" && return;
+  msg "# [SQL] WkFloGX() >> $cmd0 is NOT an Internal Command!"
+  if [[ -e $cmdGP/$cmd0 ]]; then
+    $cmdGP/$cmd0 "$@";  xc=$?; 
+	msg "# [SQL] WkFlo() >> $cmd0 - Exit Code: $xc"
+  else 
+    msg "# [SQL] ** WkFlo() >> $cmd0 is NOT an External Command! **"
+  fi
 }
+DbDmpGX () {  local dbN=$1  tmpGP; shift  
+    msg "# [SQL] DbDmpGX() >> Start: tkn: $WkFloTkn  DbN: $dbN  args: $@"
+tmpGP=$SrvGP/tmp/$dbN; mkdir -p $tmpGP;  rm -fr $tmpGP/*
+mysqldump --add-drop-table $dbN > $tmpGP/${dbN}.sql.tmp 2> $tmpGP/dqlDmp.log; xc=$? 
+msg "# [SQL] DbDmpGX() - tmpGP: $tmpGP  xc: $xc"
+msg "# WfDbDmpCB $WfFloTkn $xc $tmpGP" 
+}  
+###########
 #suFifoRcv $Srv/Knz/WkFlo/srv/cmd /srv/run/wkFlo  hstWkFloRcv.fifo
 suFifoRcv /srv/lib/wkFlo/cmd /srv/run/wkFlo  WkFlo2Kn.fifo
